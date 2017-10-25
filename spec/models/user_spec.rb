095f67bf1809252fdfb3e264989a6248ff1d4f13
Matt Hoover <matt@hoover.ml>
2017-10-25 10:03:13 +0000

    Add test for user creation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+    describe "creation" do
+        before do
+            @user = User.create(email: "test@test.com", 
+                                password: "foobar", 
+                                password_confirmation: "foobar")
+        end
+
+        it "can be created" do
+            expect(@user).to be_valid
+        end
+    end
 end
