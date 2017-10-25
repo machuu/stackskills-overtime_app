7cd11169ae2fb3e98a239a3cb65c916e1269a9a0
Matt Hoover <matt@hoover.ml>
2017-10-25 10:08:12 +0000

    Add test that user is not valid if `first_name` and `last_name` are missing
    This test fails, because the User model does not have validations on `first_name`
    or `last_name` yet.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,21 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
     describe "creation" do
         before do
-            @user = User.create(email: "test@test.com", 
-                                password: "foobar", 
-                                password_confirmation: "foobar")
+            @user = User.create(email: "test@test.com",
+                                password: "foobar",
+                                password_confirmation: "foobar",
+                                first_name: "test",
+                                last_name: "user")
         end
 
         it "can be created" do
             expect(@user).to be_valid
+        end
+
+        it "cannot be created without first_name, last_name" do
+           @user.first_name = nil
+           @user.last_name = nil
+           expect(@user).to_not be_valid
         end
     end
 end
