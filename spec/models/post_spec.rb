d3486e5b1d4a962ec45813381b9b4876b10acdc2
Matt Hoover <matt@hoover.ml>
2017-10-29 10:50:05 +0000

    Add "creation" test for Post object

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Creation" do
+    before do
+      @post = Post.create(date: Date.today, rationale: "Anything")
+    end
+
+    it "can be created" do
+      expect(@post).to be_valid
+    end
+  end
 end
