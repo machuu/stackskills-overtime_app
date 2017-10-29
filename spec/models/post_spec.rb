f3463238432380d5b4a90c7620e9f4012d8a5a5c
Matt Hoover <matt@hoover.ml>
2017-10-29 10:55:22 +0000

    Add test and validation for new Post requiring `date` and `rationale`

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -9,5 +9,11 @@ RSpec.describe Post, type: :model do
     it "can be created" do
       expect(@post).to be_valid
     end
+
+    it "cannot be created without a date and rationale" do
+      @post.date = nil
+      @post.rationale = nil
+      expect(@post).to_not be_valid
+    end
   end
 end
