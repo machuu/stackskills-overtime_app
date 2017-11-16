4655d1ec3db43ea4d5aad8f759a52927bbe0edfa
Matt Hoover <matt@hoover.ml>
2017-11-16 10:31:03 +0000

    Add `user` login to post tests

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -15,6 +15,12 @@ describe 'navigate' do
 
   describe 'creation' do
     before do
+      @user = User.create(email: "test@test.com",
+                          password: "foobar",
+                          password_confirmation: "foobar",
+                          first_name: "test",
+                          last_name: "user")
+      login_as(user, scope: user)
       visit new_post_path
     end
 
@@ -29,6 +35,15 @@ describe 'navigate' do
       click_on 'Save'
 
       expect(page).to have_content("some rationale")
+    end
+
+    it 'will have a user associated with it' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: 'User Association'
+
+      click_on 'Save'
+
+      expect(User.last.posts.last.rationale).to eq("User Association")
     end
   end
 end
