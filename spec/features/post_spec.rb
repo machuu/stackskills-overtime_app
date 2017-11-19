f16f37b568380870fdef2dc135fcd1e4fa0c50f2
Matt Hoover <matt@hoover.ml>
2017-11-19 11:08:27 +0000

    Refactor 'index' tests
    `visit posts_path` before each `index` test, and remove duplicate calls
    in each test

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -11,13 +11,15 @@ describe 'navigate' do
   end
 
   describe 'index' do
-    it "can be reached" do
+    before do
       visit posts_path
+    end
+
+    it "can be reached" do
       expect(page.status_code).to eq(200)
     end
 
     it "has a title of posts" do
-      visit posts_path
       expect(page).to have_content(/Posts/)
     end
 
