f44a2557963b7af58768b84195d803254fb47338
Matt Hoover <matt@hoover.ml>
2017-11-19 11:55:09 +0000

    Add a user to `seeds.rb` and use it for posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
+@user = User.create(email: "test@test.com",
+                   password: "foobar",
+                   password_confirmation: "foobar",
+                   first_name: "test",
+                   last_name: "user")
+
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content")
+  Post.create!(date: Date.today,
+               rationale: "#{post} rationale content",
+               user_id: @user.id)
 end
 
 puts "100 posts have been created"
