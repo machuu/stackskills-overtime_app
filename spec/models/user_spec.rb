0635fd67dee4039a6cd7a53e57c9bd145d71d894
Matt Hoover <matt@hoover.ml>
2017-11-20 11:19:54 +0000

    Move user creation in `user_spec` so it is run for all `describe` blocks

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    describe "creation" do
-        before do
-            @user = User.create(email: "test@test.com",
-                                password: "foobar",
-                                password_confirmation: "foobar",
-                                first_name: "test",
-                                last_name: "user")
-        end
+    before do
+        @user = User.create(email: "test@test.com",
+                            password: "foobar",
+                            password_confirmation: "foobar",
+                            first_name: "test",
+                            last_name: "user")
+    end
 
+    describe "creation" do
         it "can be created" do
             expect(@user).to be_valid
         end
