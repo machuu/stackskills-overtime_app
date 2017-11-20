02c20070441f8be87585768e29c2f04340e3b422
Matt Hoover <matt@hoover.ml>
2017-11-20 11:22:19 +0000

    Fix indentation in `user_spec`

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,22 +2,22 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
     before do
-        @user = User.create(email: "test@test.com",
-                            password: "foobar",
-                            password_confirmation: "foobar",
-                            first_name: "test",
-                            last_name: "user")
+      @user = User.create(email: "test@test.com",
+                          password: "foobar",
+                          password_confirmation: "foobar",
+                          first_name: "test",
+                          last_name: "user")
     end
 
     describe "creation" do
-        it "can be created" do
-            expect(@user).to be_valid
-        end
+      it "can be created" do
+          expect(@user).to be_valid
+      end
 
-        it "cannot be created without first_name, last_name" do
-           @user.first_name = nil
-           @user.last_name = nil
-           expect(@user).to_not be_valid
-        end
+      it "cannot be created without first_name, last_name" do
+         @user.first_name = nil
+         @user.last_name = nil
+         expect(@user).to_not be_valid
+      end
     end
 end
