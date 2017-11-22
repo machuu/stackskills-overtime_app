6a10f384909f43a2f534d39d704ec48036cd74ad
Matt Hoover <matt@hoover.ml>
2017-11-22 11:24:11 +0000

    Refactor `update` action to handle update failure

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,11 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Your Post was successfully edited'
+    else
+      render :edit
+    end
   end
 
   def create
