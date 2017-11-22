5ba5c8f527e657c12bf36b1e0481aec6db77fadc
Matt Hoover <matt@hoover.ml>
2017-11-22 10:50:16 +0000

    Add `edit` action and view template to PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show]
+  before_action :set_post, only: [:show, :edit]
 
   def index
     @posts = Post.all
@@ -7,6 +7,9 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+  end
+
+  def edit
   end
 
   def create
