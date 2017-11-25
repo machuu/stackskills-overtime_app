ea28027d6441de095a39fbccdd77aec8e380afa5
Matt Hoover <matt@hoover.ml>
2017-11-25 11:22:00 +0000

    Add and Resolve Test that non Admin Users are redirected from Admin Dashboard to root

--- spec/features/admin_dashboard_spec.rb
+++ spec/features/admin_dashboard_spec.rb
@@ -10,4 +10,13 @@ describe 'admin dashboard' do
     visit admin_root_path
     expect(current_path).to eq(new_user_session_path)
   end
+
+  it 'can not be reached by a non admin user' do
+    user = FactoryBot.create(:user)
+    login_as(user, scope: :user)
+
+    visit admin_root_path
+
+    expect(current_path).to eq(root_path)
+  end
 end
