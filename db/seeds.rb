8b9b9f7d539396ac5ffd4c327d06c3b5dd2ab33c
Matt Hoover <matt@hoover.ml>
2017-11-29 10:36:12 +0000

    Create an Admin User in Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,14 @@
                    password_confirmation: "foobar",
                    first_name: "test",
                    last_name: "user")
+puts "1 User Created"
+
+@admin_user = AdminUser.create(email: "admin@test.com",
+                   password: "foobar",
+                   password_confirmation: "foobar",
+                   first_name: "admin",
+                   last_name: "user")
+puts "1 Admin User Created"
 
 100.times do |post|
   Post.create!(date: Date.today,
