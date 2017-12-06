0b15beb81e2ae73a82f830f44966df909e1c9868
Matt Hoover <matt@hoover.ml>
2017-12-06 10:18:18 +0000

    Refactor Approval Workflow spec
    Move `visit edit_post_path()` to `before` block

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -11,20 +11,19 @@ describe 'navigate' do
   describe 'edit' do
     before do
       @post = FactoryBot.create(:post, user: user)
+      visit edit_post_path(@post)
     end
 
     it 'has a status that can be edited on the form by an admin' do
-      visit edit_post_path(@post)
-
       choose 'post_status_approved'
       click_on 'Save'
+
       expect(@post.reload.status).to eq("approved")
     end
 
     it 'can not be edited by a non-admin' do
       logout(:admin_user)
       login_as(user, scope: :user)
-
       visit edit_post_path(@post)
 
       expect(page).to_not have_content('Approved')
